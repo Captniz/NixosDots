32975a065be14623efb89eaa575b971f1c8abb23
Simone Tardiani <simone.tardiani.imp@gmail.com>
2024-04-22 09:48:07 +0200

    Configured alacritty

--- home.nix
+++ home.nix
@@ -254,6 +254,27 @@
       enable = true;
     };
 
+    alacritty = {
+      enable = true;
+      settings = {
+        font = {
+          normal = {
+            family = "FiraCode Nerd Font";
+            style = "Retina";
+          };
+          bold = {
+            family = "FiraCode Nerd Font";
+            style = "Bold";
+          };
+          italic = {
+            family = "FiraCode Nerd Font";
+            style = "Italic";
+          };
+          size = 12;
+        };
+      };
+    };
+
     zathura = {
       enable = true;
       options = {
