8aee8056b9f13a27df93f0b6762143cdc1c7c5f9
Simone Tardiani <simone.tardiani.imp@gmail.com>
2024-05-27 22:24:53 +0200

    Cleaned home

--- home.nix
+++ home.nix
@@ -2,7 +2,10 @@
 
 {
   imports = [ 
+    # General Settings
     ./User/Settings/Style.nix
+
+    # App settings
     ./User/App/Hyprland.nix
     ./User/App/Dunst.nix
     ./User/App/Rofi.nix
@@ -18,21 +21,8 @@
   # Home config
   home.username = "simo";
   home.homeDirectory = "/home/simo";
-  
   home.stateVersion = "23.11"; # Please read the comment before changing.
-  
-  home.packages = with pkgs;  [
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
+  programs.home-manager.enable = true;
   
   nixpkgs = {
     config = {
@@ -41,19 +31,5 @@
     };
   };
 
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   gtk.enable = true;
-  programs.home-manager.enable = true;
 }
