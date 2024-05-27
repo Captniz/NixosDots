5341c924d1a34228d5e2c28b8cbc3fbaf7b719ba
Simone Tardiani <simone.tardiani.imp@gmail.com>
2024-05-27 22:09:52 +0200

    updated zsh module

--- User/App/Zsh.nix
+++ User/App/Zsh.nix
@@ -34,4 +34,33 @@
       add_newline = false;
     };
   };
+
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      clr = "clear";
+      rcat = "cat";
+      psqlogin = "sudo -u postgres psql";
+      cat = "bat";
+      gacp = "git add * ; git commit --all -m '.' ; git push --all";
+      flakeup = "nix flake update /etc/nixos";
+      homeup = "home-manager switch --flake /etc/nixos --impure";
+      nixup = "sudo nixos-rebuild switch";
+      nixconfigure = "code /etc/nixos";
+      nixdeleteolder = "sudo nix-collect-garbage --delete-older-than";
+      zip = "7z a -r -tzip";
+    };
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "z"
+        "starship"
+      ];
+    };
+  };
 }
