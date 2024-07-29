db594c7cafec579a2ae737a9fd772c0fccb8b834
Simone Tardiani <simone.tardiani.imp@gmail.com>
2024-07-29 17:36:31 +0200

    Updated icon theme

--- User/Settings/Style.nix
+++ User/Settings/Style.nix
@@ -3,12 +3,26 @@
 {
   home.packages = with pkgs;  [
     (nerdfonts.override { fonts = [ "FiraCode" "Iosevka" "Meslo" ]; })  # Nerd Fonts
-    bibata-cursors                                              # Cursor theme         
+    bibata-cursors                                                      # Cursor theme
+    papirus-icon-theme                                                  # Icon theme         
+    gruvbox-gtk-theme                                                   # Gtk theme
   ];
   home.pointerCursor = {
     gtk.enable = true;
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
     size = 12;
+  };
+
+  gtk={ 
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus";
+    };
+
+    theme = {
+      name = "Gruvbox theme";
+      package = pkgs.gruvbox-gtk-theme;
+    };
   };
 }
