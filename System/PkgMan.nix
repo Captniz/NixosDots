11cd12d240eb6eb224beaa5394e2a481da8e6902
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-01-06 16:09:45 +0100

    Fixed an error

--- System/PkgMan.nix
+++ System/PkgMan.nix
@@ -15,7 +15,6 @@
   environment.systemPackages = with pkgs; [
     # Testing
 
-    google-chrome
     #yazi                # Terminal file manager
     #imagemagick         # Image manipulation
     #ripgrep             # Ripgrep
