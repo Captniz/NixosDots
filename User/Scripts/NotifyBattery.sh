a234c8d6e77b3e32793ff7da2e3636ba6ab6e280
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-09-24 17:04:55 +0200

    Created a script to notify low battery

---
+++ User/Scripts/NotifyBattery.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+BATTERY_PATH="/org/freedesktop/UPower/devices/battery_BAT1"
+
+
+# Function to send notifications via dunst
+send_notification() {
+    local urgency=$1
+    local icon=$2
+    local title=$3
+    local message=$4
+    notify-send -u "$urgency" -i "$icon" "$title" "$message"
+}
+
+# Listen to DBus PropertiesChanged for the battery
+dbus-monitor --system "interface='org.freedesktop.DBus.Properties',member='PropertiesChanged',path='$BATTERY_PATH'" |
+while read -r line; do
+
+    # When a property changes, get current values
+    if echo "$line" | grep -qE "percentage|state"; then
+        BATTERY_PERCENT=$(upower -i "$BATTERY_PATH" | awk '/percentage/ {gsub("%",""); print $2}')
+
+        # Battery percentage notifications
+        if [ "$BATTERY_PERCENT" -eq 25 ]; 
+        then
+            send_notification "normal" "battery-caution-symbolic" "Battery Low" "$BATTERY_PERCENT% remaining"
+
+        elif [ "$BATTERY_PERCENT" -eq 10 ] ; 
+        then
+            send_notification "critical" "battery-caution-symbolic" "Battery Critical" "Only $BATTERY_PERCENT% remaining!"
+        
+        elif [ "$BATTERY_PERCENT" -eq 5 ];
+        then
+            send_notification "critical" "battery-empty" "Hibernation Imminent" "Only $BATTERY_PERCENT% remaining! Will soon hibernate!"
+        
+        fi
+    fi
+done
