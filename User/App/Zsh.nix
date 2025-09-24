d0b6ab8b2025e7e1fca5f547ba288630f87014af
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-09-24 11:55:43 +0200

    formatting

--- User/App/Zsh.nix
+++ User/App/Zsh.nix
@@ -154,6 +154,7 @@ in
       
       #! Hasn't been necessary recently 
       # fixaudio = "systemctl restart --user pipewire.service";
+      
       systemupdate = "cd /etc/nixos;cp flake.lock bu-flake.lock;flakeup;git add *;git commit --all -m '!Breaking changes! System updated';git push;homeup;nixup";
       unmount = "sudo umount -f /mnt/ExtDrive";
     };
