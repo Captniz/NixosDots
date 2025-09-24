e250c50bf059c42b17b17109ba6204d8e7786df9
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-09-24 17:09:13 +0200

    Set up hypridle

--- User/App/HyprIdle.nix
+++ User/App/HyprIdle.nix
@@ -10,23 +10,29 @@
   services.hypridle = {
     enable = true;
     settings = {
-        general = {
-          after_sleep_cmd = "hyprctl dispatch dpms on";
-          ignore_dbus_inhibit = false;
-          lock_cmd = "hyprlock";
+      general = {
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
+        lock_cmd = "pidof hyprlock || hyprlock";
+        unlock_cmd = "dunstify -t 600 'Bentornato'";
       };
 
-    listener = [
-      {
-        timeout = 900;
-        on-timeout = "hyprlock";
-      }
-      {
-        timeout = 1200;
-        on-timeout = "hyprctl dispatch dpms off";
-        on-resume = "hyprctl dispatch dpms on";
-      }
-    ];
+      listener = [
+        {
+          timeout = 90;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 120;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          timeout = 300;
+          on-timeout = "systemctl hybrid-sleep";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
     };
   };
 }
