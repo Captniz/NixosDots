48b31c6aeeb0f2536fae2a84c56827ae21481ab6
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-09-24 10:02:27 +0200

    Removed unused DB

--- System/Services.nix
+++ System/Services.nix
@@ -59,7 +59,6 @@
 
     postgresql = {
       enable = true;
-      ensureDatabases = [ "mydatabase" ];
       authentication = pkgs.lib.mkOverride 10 ''
         #type database  DBuser  auth-method
         local all       all     trust
