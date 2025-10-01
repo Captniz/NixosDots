c58e5b5e3472b732a6c3e66cf38581fc2a71f410
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-10-01 11:25:48 +0200

    Enhance Git configuration: remove credential helper, add SSH settings, and streamline URL mappings for GitHub and GitLab

--- User/App/Git.nix
+++ User/App/Git.nix
@@ -15,9 +15,35 @@
     extraConfig = {
       init.defaultBranch = "main";
       safe.directory = "/etc/nixos";
-      credential.helper = "oauth";
       pull.rebase = false;
       http.postBuffer = 524288000;
+      url."git@github.com:".insteadOf = "https://github.com/";
+      url."git@gitlab.com:".insteadOf = "https://gitlab.com/";
+    };
+  };
+
+  programs.ssh = {
+    enable = true;
+
+    # Opt-out of legacy defaults
+    enableDefaultConfig = false;
+
+    # Define defaults yourself if you want them
+    matchBlocks = {
+      "*" = {
+        forwardAgent = true;
+        identityFile = "~/.ssh/id_ed25519";
+      };
+
+      "github.com" = {
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+
+      "gitlab.com" = {
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
     };
   };
 }
