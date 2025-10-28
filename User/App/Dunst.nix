c1be3e3d711fa40ca8678d881d10db417c04835c
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-10-29 00:14:13 +0100

    small fix

--- User/App/Dunst.nix
+++ User/App/Dunst.nix
@@ -45,7 +45,6 @@ in
 
         # Xwayland
         layer = "overlay";
-        #force_xwayland = true;
 
         # Icons
         icon_path = "${pkgs.papirus-icon-theme}/share/icons/breeze-dark/status/16";
