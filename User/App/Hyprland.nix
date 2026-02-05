5c08b3ca94391057189f6fa33ec5e248136649ab
Simone Tardiani <simone.tardiani.imp@gmail.com>
2026-02-05 16:19:58 +0100

    Added descriptions to hyprland macros

--- User/App/Hyprland.nix
+++ User/App/Hyprland.nix
@@ -106,81 +106,77 @@ in
         preserve_split = "yes";
       };
 
-      bind = [
+      bindd = [
         # Lauchers
-        "SUPER, RETURN, exec, alacritty --class Term"
-        "SUPER, R, exec, [float; center] rofi -show drun"
-        "SUPER, F, exec, zen-beta"
-        "SUPER, A, exec, code"
-        "SUPER, Q, exec, rofi -show obsidian"
-        "SUPER, S, exec, grim -g \"$(slurp)\" - | swappy -f -"
-        "SUPER, E, exec, alacritty --class Yazi -e yazi "
-        "SUPER, B, exec, alacritty --class Btop -e btop"
-        "SUPER, code:60, exec, rofi -show emoji"
-        "SUPER, N, exec, rofi -show calc"
-        "SUPER, L, exec, rofi -show p -modi p:\'/usr/bin/env bash \"${userSettings.scriptsPath}/PowerMenu.sh\"\'"
-        "SUPER, I, exec, rofi -show nerdy"
-        "SUPER, W, exec, sh ${userSettings.scriptsPath}/NixActions.sh"
-        "SUPER, V, exec, cliphist list | rofi -dmenu | cliphist decode | wl-copy"
+        "SUPER, RETURN, New Terminal, exec, alacritty --class Term"
+        "SUPER, R, Show App Launcher, exec, [float; center] rofi -show drun"
+        "SUPER, F, Open Browser,exec, zen-beta"
+        "SUPER, A, Open IDE,exec, code"
+        "SUPER, Q, Open Obsidian, exec, rofi -show obsidian"
+        "SUPER, S, Screenshot, exec, grim -g \"$(slurp)\" - | swappy -f -"
+        "SUPER, E, Open File-manager, exec,alacritty --class Yazi -e yazi "
+        "SUPER, B, Show System Performance, exec,alacritty --class Btop -e btop"
+        "SUPER, code:60, Open Emoji selector, exec,rofi -show emoji"
+        "SUPER, N, Open calculator, exec ,rofi -show calc"
+        "SUPER, L, Show Power-menu, exec,rofi -show p -modi p:\'/usr/bin/env bash \"${userSettings.scriptsPath}/PowerMenu.sh\"\'"
+        "SUPER, I, Open Icon selector, exec,rofi -show nerdy"
+        "SUPER, W, Show System actions menu, exec,sh ${userSettings.scriptsPath}/NixActions.sh"
+        "SUPER, V, Show Clipboard history, exec ,cliphist list | rofi -dmenu | cliphist decode | wl-copy"
 
         # Window managment
-        "SUPER, TAB, exec, sh ${userSettings.scriptsPath}/WindowSwitcher.sh"
-        "SUPER, J, togglesplit,"
-        "SUPER, left, movefocus, l"
-        "SUPER, right, movefocus, r"
-        "SUPER, up, movefocus, u"
-        "SUPER, down, movefocus, d"
-        "SUPER, C, killactive,"
-        "SUPER, X, togglefloating,"
-        "SUPER, P, pin,activewindow"
-        "SUPER, SPACE, fullscreen"
-        "SUPER, M, exit,"
+        "SUPER, TAB, Show open Windows , exec, sh ${userSettings.scriptsPath}/WindowSwitcher.sh"
+        "SUPER, J, Toggle window Tile Splitting,togglesplit,"
+        "SUPER, left,Move focus to the left, movefocus, l"
+        "SUPER, right,Move focus to the right, movefocus, r"
+        "SUPER, up,Move focus upward, movefocus, u"
+        "SUPER, down, Move focus downward,movefocus, d"
+        "SUPER, C, Close focused window , killactive,"
+        "SUPER, X,Toggle floating on focused window, togglefloating,"
+        "SUPER, P, Pin focused window,pin,activewindow"
+        "SUPER, SPACE,Toggle fulscreen on focused window, fullscreen"
+        "SUPER, M, Exit to TTY, exit,"
 
         # Workspaces
-        "SUPER, 1, workspace, 1"
-        "SUPER, 2, workspace, 2"
-        "SUPER, 3, workspace, 3"
-        "SUPER, 4, workspace, 4"
-        "SUPER, 5, workspace, 5"
-        "SUPER, 6, workspace, 6"
-        "SUPER, 7, workspace, 7"
-        "SUPER, 8, workspace, 8"
-        "SUPER, 9, workspace, 9"
-        "SUPER, 0, workspace, 10"
+        "SUPER, 1,Move to workspace 1, workspace, 1"
+        "SUPER, 2,Move to workspace 2, workspace, 2"
+        "SUPER, 3,Move to workspace 3, workspace, 3"
+        "SUPER, 4,Move to workspace 4, workspace, 4"
+        "SUPER, 5,Move to workspace 5, workspace, 5"
+        "SUPER, 6,Move to workspace 6, workspace, 6"
+        "SUPER, 7,Move to workspace 7, workspace, 7"
+        "SUPER, 8,Move to workspace 8, workspace, 8"
+        "SUPER, 9,Move to workspace 9, workspace, 9"
+        "SUPER, 0,Move to workspace 10, workspace, 10"
 
         # Move active window to a workspace
-        "SUPER SHIFT, 1, movetoworkspace, 1"
-        "SUPER SHIFT, 2, movetoworkspace, 2"
-        "SUPER SHIFT, 3, movetoworkspace, 3"
-        "SUPER SHIFT, 4, movetoworkspace, 4"
-        "SUPER SHIFT, 5, movetoworkspace, 5"
-        "SUPER SHIFT, 6, movetoworkspace, 6"
-        "SUPER SHIFT, 7, movetoworkspace, 7"
-        "SUPER SHIFT, 8, movetoworkspace, 8"
-        "SUPER SHIFT, 9, movetoworkspace, 9"
-        "SUPER SHIFT, 0, movetoworkspace, 10"
-
-        # Scroll through existing workspaces
-        "SUPER, mouse_down, workspace, e+1"
-        "SUPER, mouse_up, workspace, e-1"
+        "SUPER SHIFT, 1,Move focused window to workspace 1, movetoworkspace, 1"
+        "SUPER SHIFT, 2,Move focused window to workspace 2, movetoworkspace, 2"
+        "SUPER SHIFT, 3,Move focused window to workspace 3, movetoworkspace, 3"
+        "SUPER SHIFT, 4,Move focused window to workspace 4, movetoworkspace, 4"
+        "SUPER SHIFT, 5,Move focused window to workspace 5, movetoworkspace, 5"
+        "SUPER SHIFT, 6,Move focused window to workspace 6, movetoworkspace, 6"
+        "SUPER SHIFT, 7,Move focused window to workspace 7, movetoworkspace, 7"
+        "SUPER SHIFT, 8,Move focused window to workspace 8, movetoworkspace, 8"
+        "SUPER SHIFT, 9,Move focused window to workspace 9, movetoworkspace, 9"
+        "SUPER SHIFT, 0,Move focused window to workspace 10, movetoworkspace, 10"
       ];
 
-      binde = [
+      bindde = [
         # System settings controls
-        "SUPER, F1, exec, wpctl set-mute @DEFAULT_SINK@ toggle && sh ${userSettings.scriptsPath}/NotifyVolume.sh && qs ipc call volume updateMute"
-        "SUPER, F2, exec, wpctl set-volume @DEFAULT_SINK@ 0.05- && sh ${userSettings.scriptsPath}/NotifyVolume.sh && qs ipc call volume update"
-        "SUPER, F3, exec, wpctl set-volume @DEFAULT_SINK@ 0.05+ && sh ${userSettings.scriptsPath}/NotifyVolume.sh && qs ipc call volume update"
-        "SUPER, F5, exec, light -U 5 && sh ${userSettings.scriptsPath}/NotifyBrightness.sh && qs ipc call brightness update"
-        "SUPER, F6, exec, light -A 5 && sh ${userSettings.scriptsPath}/NotifyBrightness.sh && qs ipc call brightness update"
-        "SUPER, F7, exec, playerctl play-pause"
-        "SUPER, F8, exec, playerctl previous"
-        "SUPER, F9, exec, playerctl next"
+        "SUPER, F1, Mute playback,exec, wpctl set-mute @DEFAULT_SINK@ toggle && sh ${userSettings.scriptsPath}/NotifyVolume.sh && qs ipc call volume updateMute"
+        "SUPER, F2, Volume down,exec, wpctl set-volume @DEFAULT_SINK@ 0.05- && sh ${userSettings.scriptsPath}/NotifyVolume.sh && qs ipc call volume update"
+        "SUPER, F3, Volume up,exec, wpctl set-volume @DEFAULT_SINK@ 0.05+ && sh ${userSettings.scriptsPath}/NotifyVolume.sh && qs ipc call volume update"
+        "SUPER, F5, Brightness down,exec, light -U 5 && sh ${userSettings.scriptsPath}/NotifyBrightness.sh && qs ipc call brightness update"
+        "SUPER, F6, Brightness up,exec, light -A 5 && sh ${userSettings.scriptsPath}/NotifyBrightness.sh && qs ipc call brightness update"
+        "SUPER, F7, Audio player Resume/Pause,exec, playerctl play-pause"
+        "SUPER, F8, Audio player Previous ,exec, playerctl previous"
+        "SUPER, F9, Audio player Next,exec, playerctl next"
       ];
 
-      bindm = [
+      binddm = [
         # Move/resize windows
-        "SUPER, mouse:272, movewindow"
-        "SUPER, mouse:273, resizewindow"
+        "SUPER, mouse:272, Move window, movewindow"
+        "SUPER, mouse:273, Resize window, resizewindow"
       ];
 
       windowrule = [
