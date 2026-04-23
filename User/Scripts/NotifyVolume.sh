62b865327e99b8e1dbaf28a4fb551a79c37c105d
Simone Tardiani <simone.tardiani.imp@gmail.com>
2026-04-23 17:42:16 +0200

    small style change

--- User/Scripts/NotifyVolume.sh
+++ User/Scripts/NotifyVolume.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dunstify "$(wpctl get-volume @DEFAULT_AUDIO_SINK@)" -t 1200 -r 1
+dunstify "Volume: $(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%.0f%%", $2 * 100; if ($3) printf " %s", $3}')" -t 1200 -r 1
