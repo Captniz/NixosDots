50500c7c59e1084266cadf01e4c0d8902e08ef4b
Simone Tardiani <simone.tardiani.imp@gmail.com>
2026-09-16 17:49:05 +0200

    smal vscode  change

--- User/App/Vscode.nix
+++ User/App/Vscode.nix
@@ -27,6 +27,7 @@ in
       "python.pythonPath" = "/run/current-system/sw/bin/python";
       "python.defaultInterpreterPath" = "/run/current-system/sw/bin/python";
       "pylint.interpreter" = [ "/run/current-system/sw/bin/python" ];
+      "qt-core.showWelcomePageOnActivation" = false;
       "qt-core.additionalQtPaths" = [
         {
           "name" = "Qt-6.9.2";
